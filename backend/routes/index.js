35dedef0de3c5569a0c872be2c46b42c7d5836d8
DmitriyBor <d-borisenkov@mail.ru>
2020-07-15 04:25:31 +0300

    Переделал поключение к mongo

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const MongoClient = require("mongodb").MongoClient;
 
 
 /* GET home page. */
@@ -10,15 +9,4 @@ router.get('/', function(req, res, next) {
 
 module.exports = router;
 
-
  
-// создаем объект MongoClient и передаем ему строку подключения
-const mongoClient = new MongoClient("mongodb+srv://DmitriyBor:Xnn2qsr6VN7fFu96@adaptaionplan.koqlm.mongodb.net/<dbname>?retryWrites=true&w=majority", { useNewUrlParser: true });
-mongoClient.connect(function(err, client){
- 
-    if(err){
-        return console.log(err);
-    }
-    // взаимодействие с базой данных
-    client.close();
-});
