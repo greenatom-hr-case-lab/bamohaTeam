7e1a0e1d23949c738c62faac0055fb68e6db5bc0
Trusihin Aleksandr Alekseevich <a.trusihin@mail.ru>
2020-07-21 20:42:16 +0300

    Create logo, input-field, custom-button, sign-in-form, sign-up-form

---
+++ client/src/components/logo/logo.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './logo.styles.scss';
+
+const Logo = () => (
+    <div className='logo'>
+        
+    </div>
+)
+
+export default Logo;
---
+++ client/src/pages/registration-page/registration-page.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+import './registration-page.styles.scss';
+
+const RegistrationPage = () => (
+    <div className='registration-page'>
+        
+    </div>
+)
+
+export default RegistrationPage;
