37969401c06e358c85fd9a4fd55f4c0d9d6ca6d7
TTishchenko <tatiana@MacBook-Pro-Tatiana.local>
2020-07-23 11:35:46 +0300

    fixed bugs

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/header/header.component';
 
 import {Switch, Route} from 'react-router-dom';
 
+import './reset.css';
 import './App.css';
 
 class App extends React.Component {
