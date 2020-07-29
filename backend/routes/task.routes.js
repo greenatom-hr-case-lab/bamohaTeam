16bc52fb845a0a8b96c6ec422c6a14ac3d5350c2
ktkurt <kaito51@SEA-ZALMAN-W10.localdomain>
2020-07-29 20:39:48 +0300

    Task creation fixes

--- backend/routes/task.routes.js
+++ backend/routes/task.routes.js
@@ -16,26 +16,11 @@ router.get("/", async (req, res) => {
   });
   
 
-router.route('/createTask').post(async(req, res, next) => {
-    try {
-        const task = new Task(req.body);
-
-        await task.save((err) => {
-            if (err) return res.status(400).json({ success: false, err })
-            return res.status(200).json({ success: true })
-        })
-
-    } catch(e) {
-        res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
-    }
-});
-
-
 router.post('/createTask', function (req, res, next){
     const task = new Task( req.body);
     task.save((err) => {
         if (err) return res.json({ success: false, err: err.message }); 
-        return res.status(200).json({ message: "Новая задача добавлен", ...task.toJSON() })
+        return res.status(200).json({ message: "Новая задача добавленa", ...task.toJSON() })
         /*let hr = User.findById(plan.hr, (err, hr) => {
             hr.plans = [...hr.plans, plan._id]
             hr.save()
